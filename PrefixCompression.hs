4379cb7d9928296a8a2f70bdaa605b690e0369ff
Manoel Vilela <manoel_vilela@engineer.com>
2017-07-12 05:31:33 -0300

    Add solution for Prefix Compression

---
+++ PrefixCompression.hs
@@ -0,0 +1,33 @@
+-- PrefixCompression between two strings
+-- Solved in Wed 12 Jul 2017 05:08:22 AM -03
+-- Manoel Vilela
+
+sharedPrefix :: String -> String -> String
+sharedPrefix _ [] = []
+sharedPrefix [] _ = []
+sharedPrefix (x:xs) (y:ys)
+  | x == y    = x:sharedPrefix xs ys
+  | otherwise = sharedPrefix xs ys
+
+
+removePrefix :: String -> String -> String
+removePrefix [] s = s
+removePrefix _ [] = []
+removePrefix (p:ps) (s:ss)
+  | p == s = removePrefix ps ss
+  | otherwise = ss
+
+
+
+prefixCompression :: String -> String -> [String]
+prefixCompression r s = [showCompression x | let p = sharedPrefix r s
+                                                 x' = removePrefix p r
+                                                 y' = removePrefix p s,
+                                                 x <- [p,x',y']]
+  where showCompression x = (show $ length x) ++ " "++ x
+
+
+main :: IO()
+main = do x <- getLine
+          y <- getLine
+          mapM_ (putStrLn) $ prefixCompression x y
