6b7321f9a11c7c6cdec2b44b595c86bc5e8d41e1
Manoel Vilela <manoel_vilela@engineer.com>
2017-07-15 23:35:47 -0300

    Optimize FilterElements.hs: O(n²) => O(n*log(n))
    Passing in all tests cases. Need save initial positions of list
    on a HashMap, do group & sort, collect the length of each group,
    unsort by the previous saved initial positions and print.

--- FilterElement.hs
+++ FilterElement.hs
@@ -2,24 +2,42 @@
 -- Date: Thu 11 Jul 2017
 -- Manoel Vilela
 
-import Data.List(nub, intercalate)
+import qualified Data.Map as Map
 import Control.Monad (replicateM)
+import Data.Maybe
+import Data.List (sortBy, group, sort)
 
 -- TODO: Optimize filterElements, very slow.
 -- not passing on all tests because I got due time
-
+-- Problem: nub is O(n^2)
 hasAtLeast :: Int -> Int -> [Int] -> Bool
-hasAtLeast 0 x ys = True
-hasAtLeast k x [] = False
+hasAtLeast 0 _ _ = True
+hasAtLeast _ _ [] = False
 hasAtLeast k x (y:ys) = if x == y then hasAtLeast (k - 1) x ys
                         else hasAtLeast k x ys
 
+
+-- Lookup Map is just O(log(n))
+initialPosition :: Map.Map Int Int -> Int -> Int -> Ordering
+initialPosition m a b = lookup'
+  where v1 = fromJust $ Map.lookup a m
+        v2 = fromJust $ Map.lookup b m
+        lookup'
+          | v1 > v2 = GT
+          | v1 < v2 = LT
+          | otherwise = EQ
+
+
+-- Insertion Map.fromListWithkey is O(n*log(n))
+-- is someway, still better than use nub function whose is O(n²)
 filterElements :: Int -> [Int] -> [Int]
-filterElements k xs = filter (\x -> hasAtLeast k x xs) $ nub xs
+filterElements k xs = let positions = Map.fromListWithKey (\_ _ v -> v) $ zip xs [0..]
+                          sorted = map (\l@(x:_) -> (x,length l))$ group $ sort xs
+                      in sortBy (initialPosition positions) $ map fst $ filter (\(x, s) -> s >= k) $ sorted
 
 wrap :: [Int] -> String
 wrap [] = "-1"
-wrap xs = intercalate " " $ map show xs
+wrap xs = unwords $ map show xs
 
 
 readNumbers :: String -> [Int]
@@ -37,6 +55,6 @@ executeCase :: (Int, [Int]) -> String
 executeCase (k, xs) = wrap $ filterElements k xs
 
 main :: IO()
-main = do lines <- getLine
-          cases <- replicateM (read lines) readCase
+main = do n <- readLn
+          cases <- replicateM n readCase
           mapM_ putStrLn $ map executeCase cases
