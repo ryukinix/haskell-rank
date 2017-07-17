70adcf043ec43728c923ff3478cb0d78c676fff1
Manoel Vilela <manoel_vilela@engineer.com>
2017-07-17 12:46:36 -0300

    BirthdayCakeCandles: lint file, remove flycheck errors

--- BirthdayCakeCandles.hs
+++ BirthdayCakeCandles.hs
@@ -11,8 +11,7 @@ count k (x:xs)
 
 main :: IO ()
 main = do
-    n_temp <- getLine
-    let n = read n_temp :: Integer
+    _ <- getLine -- ignore first input
     ar_temp <- getLine
     let ar = map read $ words ar_temp :: [Integer]
     print $ count (maximum ar) ar
